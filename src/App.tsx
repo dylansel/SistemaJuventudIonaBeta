a59e0e4587680ca96da5eefe5f44bc308b4c2541
Ezequiel Binker <ezebinker@gmail.com>
2022-02-13 01:20:08 -0300

    feat: Add auth0 support and NotLoggedIn screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import './styles/App.css';
 import { Outlet } from "react-router-dom";
 import HeaderNav from './components/UI/Layout/HeaderNav';
+import NotLoggedIn from './pages/NotLoggedIn';
 
 function App() {
+  let { isAuthenticated } = useAuth0()
+  isAuthenticated = true // --> Only for testing...
+
   return (
     <>
       <header>
         <HeaderNav />
       </header>
-      <Outlet />
+      {
+        isAuthenticated ? <Outlet /> : <NotLoggedIn />
+      }
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 import MainRoutes from './routes/mainRoutes';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainRoutes />
+    <Auth0Provider
+      domain="dev-jve24r55.us.auth0.com"
+      clientId="QK1l4qzYhR7z6fGHCvo7HcyiTQmGg8K2"
+      redirectUri={window.location.origin}
+    >
+      <MainRoutes />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
