38492e88158d2b69cb97c270ba10034386ea0245
Ezequiel Binker <ezebinker@gmail.com>
2022-02-13 16:42:30 -0300

    feat: Add auth provider with history

---
+++ src/auth/auth0-provider-with-history.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+function Auth0ProviderWithHistory({ children }: any) {
+    const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+    const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+    const history = useNavigate();
+
+    const onRedirectCallback = (appState: any) => {
+        history(appState?.returnTo || window.location.pathname);
+    };
+
+    return (
+        <Auth0Provider
+            domain={process.env.REACT_APP_AUTH0_DOMAIN!}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
+            redirectUri={window.location.origin}
+            onRedirectCallback={onRedirectCallback}
+        >
+            {children}
+        </Auth0Provider>
+    );
+};
+
+export default Auth0ProviderWithHistory;
