1f9b70733f3f6dc4c0d58a4b6d69c8d9e44d242d
Dylansel <dylanseltzer4@gmail.com>
2022-02-17 19:17:47 -0300

    mejora ui

--- src/components/UI/Modals/Janijim/AddJanijBody.tsx
+++ src/components/UI/Modals/Janijim/AddJanijBody.tsx
@@ -136,7 +136,7 @@ function AddJanijBody(props: any) {
                             {(!(loaded && viewData && viewData["families"] && viewData["groups"])) &&
                                 <option disabled selected>Cargando...</option>
                             }
-                            <option key="-1" value="-1" selected >Elegir un grupo</option>
+                            <option key="-1" value="-1" selected >Elija un grupo</option>
                             {loaded && viewData && viewData["groups"].map((grupo: any) => (
                                 <option key={grupo.id} value={grupo.id}>{grupo.name}</option>
                             ))}
