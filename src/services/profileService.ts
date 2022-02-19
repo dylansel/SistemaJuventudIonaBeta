40910271aed5b6d3d38c7a7243d4ae0a2c0c0f70
Ezequiel Binker <ezebinker@gmail.com>
2022-02-19 17:39:30 -0300

    fix: Delete present and testing /profile

---
+++ src/services/profileService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import ProfileDTO from '../dtos/ProfileDTO';
+
+export async function getProfile(getAccessTokenSilently: any): Promise<ProfileDTO> {
+    try {
+        const token = await getAccessTokenSilently();
+        const response = await axios(`${process.env.REACT_APP_BACKEND_DOMAIN!}/profile/`, {
+            headers: {
+                authorization: `Bearer ${token}`,
+            }
+        })
+        const data = await response.data
+        return data
+    } catch (error: any) {
+        throw error
+    }
+}
