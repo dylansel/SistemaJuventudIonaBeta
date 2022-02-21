a0e6fe44c7f8ce3a6ae9299c13f1fe9a6d34fb30
Ezequiel Binker <ezebinker@gmail.com>
2022-02-21 14:14:53 -0300

    fix: Add activities length validation

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -6,6 +6,7 @@ import ActivityDTO from '../dtos/ActivityDTO';
 import { getAllActivities } from '../services/activityService';
 import Loading from './misc/Loading';
 import { useNavigate } from 'react-router-dom';
+import { dateToString } from '../utils/misc/strings';
 
 function Attendance() {
     const history = useNavigate();
@@ -38,34 +39,39 @@ function Attendance() {
                 <div className="justify-content-center table-content mx-3 mt-5 col-8">
 
                     {loaded ? <>
-                        {(new Date().toISOString().split('T')[0] === activities[0].date) && <div>
-                            <p>Actividad de hoy {activities[0].date} cuesta ${activities[0].individualPrice}</p>
+                        {activities.length > 0 && (new Date().toISOString().split('T')[0] === activities[0].date) && <div>
+                            <p>Actividad de hoy, {dateToString(activities[0].date)} cuesta ${activities[0].individualPrice}</p>
                         </div>}
-                        <table className="table table-hover table-responsive">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Fecha</th>
-                                    <th scope="col">Precio</th>
-                                    <th scope="col">Cargar Asistencia</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {activities
-                                    //.filter((activity: ActivityDTO) => (!((tableFilter === 'Inactivos' && area.active) || (tableFilter === 'Activos' && !area.active))))
-                                    .map((activity: ActivityDTO) => (
-                                        <tr key={activity.id}>
-                                            <td className="td-attendance">{new Date(activity.date).toLocaleDateString([], { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric'})}</td>
-                                            <td className="td-attendance">${activity.individualPrice}</td>
-                                            <td className="td-attendance">
-                                                <span>
-                                                    <button type="button" title='Cargar Asistencia' className="btn btn-danger" onClick={() => loadAttendance(activity.id)}><i className=" fas fa-arrow-right"></i></button>
-                                                </span>
-                                            </td>
+                        {
+                            activities.length > 0 ?
+
+                                <table className="table table-hover table-responsive">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Fecha</th>
+                                            <th scope="col">Precio</th>
+                                            <th scope="col">Cargar Asistencia</th>
                                         </tr>
-                                    )
-                                    )}
-                            </tbody>
-                        </table>
+                                    </thead>
+                                    <tbody>
+                                        {activities
+                                            //.filter((activity: ActivityDTO) => (!((tableFilter === 'Inactivos' && area.active) || (tableFilter === 'Activos' && !area.active))))
+                                            .map((activity: ActivityDTO) => (
+                                                <tr key={activity.id}>
+                                                    <td className="td-attendance">{dateToString(activity.date)}</td>
+                                                    <td className="td-attendance">${activity.individualPrice}</td>
+                                                    <td className="td-attendance">
+                                                        <span>
+                                                            <button type="button" title='Cargar Asistencia' className="btn btn-danger" onClick={() => loadAttendance(activity.id)}><i className=" fas fa-arrow-right"></i></button>
+                                                        </span>
+                                                    </td>
+                                                </tr>
+                                            )
+                                            )}
+                                    </tbody>
+                                </table>
+                                : <p>No hay actividades cargadas</p>
+                        }
                     </>
                         :
                         <div className="text-center">
--- src/utils/misc/strings.ts
+++ src/utils/misc/strings.ts
@@ -9,3 +9,7 @@ export const capitalizeAllWords = (str: string) => {
 export const isEmptyOrSpaces = (str : string) => {
     return (str.length === 0 || !str.trim());
 }
+
+export const dateToString = (date: string) => {
+    return new Date(date).toLocaleDateString([], { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric'})
+}
