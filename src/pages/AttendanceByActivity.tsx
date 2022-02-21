8cc23da06117716721bfe9cd72c8e9aa3282a2e0
Ezequiel Binker <ezebinker@gmail.com>
2022-02-21 14:22:43 -0300

    feat: Add getAttendancesByActivity call

--- src/pages/AttendanceByActivity.tsx
+++ src/pages/AttendanceByActivity.tsx
@@ -9,6 +9,8 @@ import Scroll from '../components/UI/Layout/Scroll';
 import { JanijAttendanceRequestDTO } from '../dtos/JanijAttendanceRequestDTO';
 import ActivityDTO from '../dtos/ActivityDTO';
 import { getActivityById } from '../services/activityService';
+import { AttendanceDTO } from '../dtos/AttendanceDTO';
+import { getAttendanceByActivity } from '../services/attendanceService';
 
 function AttendanceByActivity() {
     const history = useNavigate();
@@ -16,6 +18,7 @@ function AttendanceByActivity() {
 
     const [janijim, setJanijim] = useState<JanijDTO[]>([])
     const [activityData, setActivityData] = useState<ActivityDTO>()
+    const [activityPreviousAttendance, setActivityPreviousAttendance] = useState<AttendanceDTO[]>()
     const [loaded, setLoaded] = useState(false)
     const [changes, setChanges] = useState<JanijAttendanceRequestDTO[]>([])
 
@@ -30,6 +33,7 @@ function AttendanceByActivity() {
         setLoaded(false)
         setJanijim(await getAllJanijim("sort=group.ordinal,asc;firstName,asc;family.surname,asc"))
         setActivityData(await getActivityById(parseInt(activityId!)))
+        setActivityPreviousAttendance(await getAttendanceByActivity(parseInt(activityId!)))
         setLoaded(true)
     }
 
@@ -39,47 +43,48 @@ function AttendanceByActivity() {
 
     return (
         <main>
-            <div className="filters d-flex mx-4 align-items-center justify-content-around">
+            <div className="filters d-flex mx-4 align-items-center justify-content-center">
                 <button type="button" title='Volver' className="btn btn-danger mx-5" onClick={() => history(-1)}><i className=" fas fa-arrow-left"></i></button>
                 {loaded && <h3>Actividad del {loaded && (new Date(activityData?.date!)).toLocaleDateString([], { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric' })} </h3>}
-                <Button onClick={handleSaveAttendance} color='danger' type='button'>Guardar Asistencia</Button>
             </div>
-            <div className="justify-content-center table-content mx-3 mt-4">
-                {loaded ? <table className="table table-hover table-responsive">
-                    <thead>
-                        <tr>
-                            <th scope="col">Nombre y Apellido</th>
-                            <th scope="col">Grupo</th>
-                            <th scope="col">Presente</th>
-                            <th scope="col">Prueba</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            janijim
-                                .filter((janij: JanijDTO) => (janij.active))
-                                .map(janij => (
-                                    <tr key={janij.id}>
-                                        <td>{`${janij.name} ${janij.familySurname}`}</td>
-                                        <td>{janij.groupName}</td>
-                                        <td>
-                                            <Input
-                                                type='checkbox'
-                                            />
-                                        </td>
-                                        <td>
-                                            <Input
-                                                type='checkbox'
-                                            />
-                                        </td>
-                                    </tr>
-                                )
-                                )}
-                    </tbody>
-                </table>
+            <div className="justify-content-center table-content mx-3 mt-4 text-center">
+                {loaded ?
+                    <>
+                        <table className="table table-hover table-responsive">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre y Apellido</th>
+                                    <th scope="col">Grupo</th>
+                                    <th scope="col">Presente</th>
+                                    <th scope="col">Prueba</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {janijim
+                                    .filter((janij: JanijDTO) => (janij.active))
+                                    .map(janij => (
+                                        <tr key={janij.id}>
+                                            <td>{`${janij.name} ${janij.familySurname}`}</td>
+                                            <td>{janij.groupName}</td>
+                                            <td>
+                                                <Input
+                                                    type='checkbox' />
+                                            </td>
+                                            <td>
+                                                <Input
+                                                    type='checkbox' />
+                                            </td>
+                                        </tr>
+                                    )
+                                    )}
+                            </tbody>
+                        </table>
+                        <Button onClick={handleSaveAttendance} className='my-5' color='danger' type='button'>Grabar Asistencias</Button>
+                    </>
+
                     :
                     <div className="text-center">
-                        <h2>Cargando Lista de Janijim...</h2>
+                        <h2>Cargando Asistencias de la actividad...</h2>
                         <Spinner animation="border" className='text-danger my-2' variant="light" />
                     </div>
                 }
