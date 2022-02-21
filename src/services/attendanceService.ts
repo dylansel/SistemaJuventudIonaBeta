b47a403f9b4dfb143c85bca83156cb75e8f3f726
Ezequiel Binker <ezebinker@gmail.com>
2022-02-21 13:59:02 -0300

    feat: Add getAttendanceByActivity

--- src/services/attendanceService.ts
+++ src/services/attendanceService.ts
@@ -1,9 +1,20 @@
 import axios from 'axios';
+import { AttendanceDTO } from '../dtos/AttendanceDTO';
 import { JanijAttendanceRequestDTO } from '../dtos/JanijAttendanceRequestDTO';
 
-export async function saveAttendance(activityId: number, janijAttendanceRequest: JanijAttendanceRequestDTO[]) {
+export async function getAttendanceByActivity(activityId: number): Promise<AttendanceDTO[]> {
     try {
-        const response = await axios.put(`${process.env.REACT_APP_BACKEND_DOMAIN}/save/getAll/${activityId}`, janijAttendanceRequest)
+        const response = await axios(`${process.env.REACT_APP_BACKEND_DOMAIN}/attendance/getByActivity/${activityId}`)
+        const data = await response.data
+        return data
+    } catch (error: any) {
+        throw error
+    }
+}
+
+export async function saveAttendance(activityId: number, janijAttendancesRequest: JanijAttendanceRequestDTO[]) {
+    try {
+        const response = await axios.put(`${process.env.REACT_APP_BACKEND_DOMAIN}/attendance/save/${activityId}`, janijAttendancesRequest)
         const data = await response.data
         return data
     } catch (error: any) {
