ebdff4f494abcab6cf3c6580dba4d7a25cb8d5e1
Ezequiel Binker <ezebinker@gmail.com>
2022-04-03 17:19:12 -0300

    feat: Add design changes and fixes (not finished)

--- src/pages/PricingCases.tsx
+++ src/pages/PricingCases.tsx
@@ -46,38 +46,33 @@ const getCasesStyle = (isDraggingOver: any) => ({
 });
 
 const getItemStyle = (isDragging: any, draggableStyle: any) => ({
-    // some basic styles to make the items look a bit nicer
     userSelect: "none",
     padding: grid * 3,
     margin: `0 0 ${grid}px 0`,
 
-    // change colour if dragging
     background: isDragging ? "lightgreen" : "#EED3D3",
     fontWeight: isDragging ? "bold" : "normal",
 
-    // styles we need to apply on draggables
     ...draggableStyle
 });
 
 function PricingCases() {
-    const [groupCases, setGroupCases] = useState<PricingCaseGroupDTO[]>([])
+    const [filteredGroupCases, setFilteredGroupCases] = useState<PricingCaseGroupDTO[]>([])
     const [activePricingCases, setActivePricingCases] = useState<PricingCaseDTO[]>([])
     const [isSaving, setIsSaving] = useState<boolean>(false)
     const [loaded, setLoaded] = useState<boolean>(false)
     const [caseName, setCaseName] = useState<string>('')
     const [handleAddCaseActive, setHandleAddCaseActive] = useState<boolean>(false)
+    const [isCreating, setIsCreating] = useState<boolean>(false)
 
     const refresh = () => {
-        setLoaded(false)
         setTimeout(() => {
             setCaseName('')
             setHandleAddCaseActive(false)
-            setLoaded(true)
         }, 1);
     }
     const onDragEnd = (result: any) => {
         const { source, destination } = result;
-        // dropped outside the list
         if (!destination) {
             return;
         }
@@ -86,10 +81,10 @@ function PricingCases() {
 
         if (sInd === dInd) {
             if (sInd === -1) {
-                const items: any = reorder(groupCases, source.index, destination.index);
-                const newState: any = [...groupCases];
+                const items: any = reorder(filteredGroupCases, source.index, destination.index);
+                const newState: any = [...filteredGroupCases];
                 newState[sInd] = items;
-                setGroupCases(newState[-1]);
+                setFilteredGroupCases(newState[-1]);
             } else {
                 const items: any = reorder(activePricingCases[sInd].pricingCaseGroups, source.index, destination.index);
                 const newState: any = [...activePricingCases];
@@ -98,21 +93,21 @@ function PricingCases() {
             }
         } else {
             if (sInd === -1) {
-                const result: any = move(groupCases, activePricingCases[dInd].pricingCaseGroups, source, destination);
-                let newStateGroup: any = groupCases;
+                const result: any = move(filteredGroupCases, activePricingCases[dInd].pricingCaseGroups, source, destination);
+                let newStateGroup: any = filteredGroupCases;
                 const newStateCases: any = activePricingCases;
                 newStateGroup = result[sInd];
                 newStateCases[dInd].pricingCaseGroups = result[dInd];
-                setGroupCases(newStateGroup)
+                setFilteredGroupCases(newStateGroup)
                 setActivePricingCases(newStateCases)
 
             } else if (dInd === -1) {
-                const result: any = move(activePricingCases[sInd].pricingCaseGroups, groupCases, source, destination);
-                let newStateGroup: any = groupCases;
+                const result: any = move(activePricingCases[sInd].pricingCaseGroups, filteredGroupCases, source, destination);
+                let newStateGroup: any = filteredGroupCases;
                 const newStateCases: any = activePricingCases;
                 newStateGroup = result[dInd];
                 newStateCases[sInd].pricingCaseGroups = result[sInd];
-                setGroupCases(newStateGroup)
+                setFilteredGroupCases(newStateGroup)
                 setActivePricingCases(newStateCases)
             } else {
                 const result: any = move(activePricingCases[sInd].pricingCaseGroups, activePricingCases[dInd].pricingCaseGroups, source, destination);
@@ -124,21 +119,49 @@ function PricingCases() {
         }
     }
 
+    const filterGroupCases = async () => {
+        const groupCases = await getGroupCases()
+        let filteredGroupCases: PricingCaseGroupDTO[] = []
+        for (const groupCase of groupCases) {
+            let isInAnyPricingCase: boolean = false
+            let i = 0
+            while (!isInAnyPricingCase && i < activePricingCases.length) {
+                let j = 0
+                while (!isInAnyPricingCase && j < activePricingCases[i].pricingCaseGroups.length) {
+                    if (activePricingCases[i].pricingCaseGroups[j].group.name ===
+                        groupCase.group.name && activePricingCases[i].pricingCaseGroups[j].leadersCourse === groupCase.leadersCourse) {
+                        isInAnyPricingCase = true
+                    }
+                    else {
+                        j++
+                    }
+                }
+                i++
+            }
+            if (!isInAnyPricingCase) {
+                filteredGroupCases.push(groupCase)
+            }
+        }
+        console.log(filteredGroupCases)
+        return filteredGroupCases
+    }
+
     const handleChangeName = (e: any) => {
         setCaseName(e.target.value)
     }
 
     const handleSaveCase = (i: any) => {
-        console.log(activePricingCases[i].name)
         const newStateCases: any = activePricingCases;
         newStateCases[i].name = caseName;
         setActivePricingCases(newStateCases)
         setCaseName('')
         setHandleAddCaseActive(false)
+        setIsCreating(false)
         refresh()
     }
 
     const handleAddCase = () => {
+        setIsCreating(true)
         setHandleAddCaseActive(true);
         activePricingCases.push({
             name: ``,
@@ -149,6 +172,8 @@ function PricingCases() {
 
     const handleDeleteCase = (name: string) => {
         setActivePricingCases(activePricingCases.filter(pricingCase => pricingCase.name !== name))
+        setIsCreating(false)
+        setHandleAddCaseActive(false)
         refresh()
     }
 
@@ -165,12 +190,13 @@ function PricingCases() {
         console.log(newPricingCases)
         await savePricingCases(newPricingCases)
         setIsSaving(false)
+        window.location.reload() //Not the best way to refresh page
     }
 
     async function fetchData() {
         setLoaded(false)
         setActivePricingCases(await getActive())
-        setGroupCases(await getGroupCases())
+        setFilteredGroupCases(await filterGroupCases())
         setLoaded(true)
     }
 
@@ -186,7 +212,7 @@ function PricingCases() {
                     <div className="main-container row justify-content-center text-center px-5">
                         <DragDropContext onDragEnd={onDragEnd}>
 
-                            {groupCases &&
+                            {filteredGroupCases &&
                                 <div className='col-12 col-md-3'>
                                     <Droppable droppableId="-1">
                                         {(provided, snapshot) => (
@@ -196,7 +222,7 @@ function PricingCases() {
                                                 style={getListStyle(snapshot.isDraggingOver)}
                                             >
                                                 <h2 className='py-3'>Grupos</h2>
-                                                {groupCases.map((item, index) => (
+                                                {filteredGroupCases.map((item, index) => (
                                                     <Draggable key={index} draggableId={`g${index}`} index={index}>
                                                         {(provided, snapshot) => (
                                                             <div
@@ -222,10 +248,10 @@ function PricingCases() {
                             <div className="d-xs-none col-1"></div>
 
                             {activePricingCases &&
-                                <div className='col-12 col-md-7 padrepricingCasesActive-custom'>
+                                <div className='col-12 col-md-7 padrepricingCasesActive-custom mx-auto'>
                                     <div className='justify-content-center d-flex py-3'>
                                         <h2 className='px-5'>Casos</h2>
-                                        <button type='button' title='Agregar' className="btn btn-danger mx-2" onClick={handleAddCase} disabled={handleAddCaseActive}><i className="fas fa-plus"></i> Agregar Nuevo Caso</button>
+                                        <button type='button' title='Agregar' className="btn btn-danger mx-2" onClick={handleAddCase} disabled={isCreating}><i className="fas fa-plus"></i> Agregar Nuevo Caso</button>
                                     </div>
                                     <div className='pricingCasesActive-custom'>
                                         {activePricingCases.map((element, index) => (
@@ -238,52 +264,59 @@ function PricingCases() {
                                                         {...provided.droppableProps}
                                                     >
                                                         <div className='justify-content-center d-flex flex-row py-3 '>
+                                                            <div className='d-flex flex-row'>
+                                                                {element.name !== "" ?
+                                                                    <a data-bs-toggle="collapse" href={`#div-${element.name}`} role="button" aria-expanded="false" aria-controls={`div-${element.name}`}>
+                                                                        <h3 className='px-5'>{element.name}</h3>
+                                                                    </a>
+                                                                    : <>
+                                                                        <Input
+                                                                            type='text'
+                                                                            id={`${index}`}
+                                                                            className="w-100"
+                                                                            name={`pricingCaseName${index}`}
+                                                                            value={caseName}
+                                                                            placeholder='Nombre del caso'
+                                                                            onChange={handleChangeName}
+                                                                        />
+                                                                    </>
 
-                                                            {element.name !== "" ?
-                                                                <h3 className='px-5'>{element.name}</h3>
-                                                                : <>
-                                                                    <Input
-                                                                        type='text'
-                                                                        id={`${index}`}
-                                                                        className="w-50"
-                                                                        name={`pricingCaseName${index}`}
-                                                                        value={caseName}
-                                                                        placeholder='Nombre del caso'
-                                                                        onChange={handleChangeName}
-                                                                    />
-                                                                </>
-
-                                                            }
+                                                                }
+                                                                <a data-bs-toggle="collapse" href={`#div-${element.name}`} role="button" aria-expanded="false" aria-controls={`div-${element.name}`}>
+                                                                    <i className="fas fa-angle-down mx-5"></i>
+                                                                </a>
+                                                            </div>
                                                             <div>
                                                                 <button type='button' title={(handleAddCaseActive) ? 'Guardar' : 'Editar'} className="btn btn-danger m-1 " onClick={() => { handleSaveCase(index) }} ><i className="fas fa-edit"></i></button>
                                                                 <button type='button' title='Eliminar' className="btn btn-danger" onClick={() => { handleDeleteCase(element.name) }} ><i className="fas fa-trash"></i></button>
                                                             </div>
 
                                                         </div>
+                                                        <div className="collapse show" id={`div-${element.name}`}>
+                                                            {element.pricingCaseGroups.map((item, index) => (
+                                                                <Draggable
+                                                                    draggableId={`${item.group.name + index}`}
+                                                                    index={index}
+                                                                >
+                                                                    {(provided, snapshot) => (
 
-                                                        {element.pricingCaseGroups.map((item, index) => (
-                                                            <Draggable
-                                                                draggableId={`${item.group.name + index}`}
-                                                                index={index}
-                                                            >
-                                                                {(provided, snapshot) => (
+                                                                        <div
+                                                                            ref={provided.innerRef}
+                                                                            {...provided.draggableProps}
+                                                                            {...provided.dragHandleProps}
+                                                                            style={getItemStyle(
+                                                                                snapshot.isDragging,
+                                                                                provided.draggableProps.style
+                                                                            )}
+                                                                        >
+                                                                            {item.group.name + (item.leadersCourse ? " (Curso)" : "")}
+                                                                        </div>
+                                                                    )}
 
-                                                                    <div
-                                                                        ref={provided.innerRef}
-                                                                        {...provided.draggableProps}
-                                                                        {...provided.dragHandleProps}
-                                                                        style={getItemStyle(
-                                                                            snapshot.isDragging,
-                                                                            provided.draggableProps.style
-                                                                        )}
-                                                                    >
-                                                                        {item.group.name + (item.leadersCourse ? " (Curso)" : "")}
-                                                                    </div>
-                                                                )}
-
-                                                            </Draggable>
-                                                        ))}
-                                                        {provided.placeholder}
+                                                                </Draggable>
+                                                            ))}
+                                                            {provided.placeholder}
+                                                        </div>
                                                     </div>
                                                 )}
                                             </Droppable>
@@ -293,19 +326,19 @@ function PricingCases() {
                                         onClick={handleSaveCases}
                                         className='my-3'
                                         color={isSaving ? 'success' : 'danger'}
-                                        disabled={isSaving || activePricingCases.length === 0 || handleAddCaseActive}
+                                        disabled={isSaving || activePricingCases.length === 0 || isCreating}
                                         type='button'
                                     >{isSaving ? <div>Guardando... <Spinner animation="border" variant="light" size="sm" /></div> : "Guardar Casos de Precios"}</Button>
 
                                 </div>
                             }
                         </DragDropContext>
-                    </div>
+                    </div >
                     :
                     <Loading />
             }
             <Scroll showBelow={250} />
-        </main>
+        </main >
     );
 }
 
