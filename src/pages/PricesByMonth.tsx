6fa73a0ac79538496ff37b27e929a030186826d8
dylan sel <dylanseltzer4@gmail.com>
2022-04-23 09:49:20 -0300

    mejorasFamilias-caseNames

--- src/pages/PricesByMonth.tsx
+++ src/pages/PricesByMonth.tsx
@@ -21,6 +21,35 @@ function PricesByMonth() {
         //TODO: Save
         setIsSaving(false)
     }
+    const countRepeated = (array:any[], elemento:any) =>{
+            let cant=0;
+            for(let i=0;i<array.length;i++){
+            if(array[i] == elemento){
+            cant++;
+            }}
+            return cant;
+    }
+    const listCaseNames = (caseNamesE:any[])=>{
+        let caseNames:any = []
+        caseNamesE.map((e)=>caseNames.push(e.name))
+        let result:any = new Set(caseNames)
+        let caseNamesLimpio:any[] = [...result]
+
+        for(let i = 0; i<caseNamesLimpio.length;i++){
+            let cant =  countRepeated(caseNames,caseNamesLimpio[i])
+            caseNamesLimpio[i] =  caseNamesLimpio[i] + ((cant>1)?` X ${cant}`:``);
+        }
+        return caseNamesLimpio
+    }
+    const listFamilyToString =  (families:string[])=>{
+        let string = "";
+        for (let i = 0; i < families.length; i++) {
+            families[i] = families[i][0].toUpperCase() + families[i].slice(1);
+        }
+        families.sort();
+        families.map((n,i)=>string += n + ((i!=families.length-1)?" | ":""))
+        return string
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -57,23 +86,22 @@ function PricesByMonth() {
                             caseCombinations.length > 0 ?
                                 <>
                                     <div className="accordion" id="caseCombinations">
-                                        {caseCombinations.map((caseCombination: CaseCombinationDTO) => {
+                                        {caseCombinations.map((caseCombination: CaseCombinationDTO,index) => {
                                             { i++ }
-                                            return <div className="accordion-item col-md-8 col-10 d-inline-block pb-3">
+                                            return <div key={index}className="accordion-item col-md-8 col-10 d-inline-block pb-3">
                                                 <p>Combinación # {i}</p>
-                                                {caseCombination.pricingCases.map((pricingCase: PricingCaseDTO) =>
-                                                    <p>{pricingCase.name}</p>
+                                                {listCaseNames(caseCombination.pricingCases).map((element:any,ind:number) =>
+                                                    <p>{element}</p> 
                                                 )}
+
                                                 <h2 className="accordion-header" id="headingOne">
-                                                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${i}`} aria-expanded="true" aria-controls={`#collapse${i}`}>
-                                                        Familias relacionadas
+                                                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${i}`} aria-expanded="false" aria-controls={`#collapse${i}`}>
+                                                        Familias relacionadas ({caseCombination.families.length})
                                                     </button>
                                                 </h2>
-                                                <div id={`collapse${i}`} className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#caseCombinations">
+                                                <div id={`collapse${i}`} className="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#caseCombinations">
                                                     <div className="accordion-body">
-                                                        {caseCombination.families.map((family: string) =>
-                                                            <p>{family}</p>
-                                                        )}
+                                                        {listFamilyToString(caseCombination.families)}
                                                     </div>
                                                 </div>
                                                 <div className="input-group flex-nowrap">
