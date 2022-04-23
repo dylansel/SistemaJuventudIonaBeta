51108a133f1f78aaa33059f5a00c3cdb15471e5e
Ezequiel Binker <ezebinker@gmail.com>
2022-04-23 19:04:34 -0300

    Fixing issues with dtos

--- src/pages/PricesByMonth.tsx
+++ src/pages/PricesByMonth.tsx
@@ -5,6 +5,7 @@ import { Button, Spinner } from 'reactstrap';
 import Scroll from '../components/UI/Layout/Scroll';
 import DialogBox from '../components/UI/Modals/DialogBox';
 import { useCallbackPrompt } from '../customHooks/useCallbackPrompts';
+import PriceCombinationCaseDTO from '../dtos/PriceCombinationCaseDTO';
 import { PriceDTO } from '../dtos/PriceDTO';
 import { PriceRequestDTO } from '../dtos/PriceRequestDTO';
 import PricingCaseDTO from '../dtos/PricingCaseDTO';
@@ -27,13 +28,12 @@ function PricesByMonth() {
     const [showPrompt, confirmNavigation, cancelNavigation] =
         useCallbackPrompt(showDialog)
 
-    const handleChange = (e: any, caseCombinationId: number, pricingCases: PricingCaseDTO[]) => {
+    const handleChange = (e: any, caseCombinationId: number, pricingCases: PriceCombinationCaseDTO[]) => {
         setHasUnsavedChanges(true)
         const newPrice = {
             month: month!,
             amount: e.target.value,
-            pricingCases: [1] /*pricingCases.map((pricingCase: PricingCasePriceDTO) => pricingCase.id)*/
-            //TODO: Problema con los DTOs... No conocemos los id de los pricing cases acá. Debe estar mal algún formato de DTO respecto de lo que viene del backend. 
+            pricingCases: pricingCases.map((pricingCase: PriceCombinationCaseDTO) => pricingCase.id)
         }
         pricesToAdd[caseCombinationId] = newPrice
         setCompletedAllPrices(pricesToAdd.every(price => price.amount > 0) && pricesToAdd.length === caseCombinations.length)
@@ -97,8 +97,6 @@ function PricesByMonth() {
         setShowDialog(hasUnsavedChanges)
     }, [hasUnsavedChanges])
 
-    let i = 0
-
     return (
         <main>
             {month &&
@@ -113,9 +111,10 @@ function PricesByMonth() {
                                             <p>Id: {price.id}</p>
                                             <p>Casos de Precio:</p>
                                             {
-                                                price.pricingCasePrices.map((price: PricingCasePriceDTO) =>
-                                                    <p>{price.pricingCase.name}</p>
+                                                price.pricingCasePrices!.map((pricingCase: PricingCasePriceDTO) =>
+                                                    <p>{pricingCase.pricingCase.name}</p>
                                                 )
+                                                //TODO: Reutilizar la función de listCaseName acá.
                                             }
                                             <p>------</p>
                                             <p className='fw-bold'>Precio: ${price.amount}</p>
@@ -131,19 +130,18 @@ function PricesByMonth() {
                                 <>
                                     <div className="accordion" id="caseCombinations">
                                         {caseCombinations.map((caseCombination, index) => {
-                                            caseCombination.id = i
-                                            i++
+                                            caseCombination.id = index
                                             return <div key={index} className="accordion-item col-md-8 col-10 d-inline-block p-3">
                                                 <div className='col-10 d-inline-block'>
-                                                    <p>Combinación # {i}</p>
+                                                    <p>Combinación # {index + 1}</p>
                                                     <p className='fw-bold'>{listCaseNames(caseCombination.pricingCases)}</p>
 
                                                     <h2 className="accordion-header" id="headingOne">
-                                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${i}`} aria-expanded="false" aria-controls={`#collapse${i}`}>
+                                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${index + 1}`} aria-expanded="false" aria-controls={`#collapse${index + 1}`}>
                                                             Familias relacionadas ({caseCombination.families.length})
                                                         </button>
                                                     </h2>
-                                                    <div id={`collapse${i}`} className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#caseCombinations">
+                                                    <div id={`collapse${index + 1}`} className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#caseCombinations">
                                                         <div className="accordion-body">
                                                             {listFamilyToString(caseCombination.families)}
                                                         </div>
@@ -151,7 +149,7 @@ function PricesByMonth() {
                                                     <div className="d-inline-block col-md-6 mt-3">
                                                         <div className="input-group flex-nowrap">
                                                             <span className="input-group-text" id="addon-wrapping">Precio $</span>
-                                                            <input type="number" id={`priceCase${i}`} name={`priceCase${i}`} onChange={(e) => handleChange(e, caseCombination.id, caseCombination.pricingCases)} className="form-control" placeholder="3000" aria-label="Username" aria-describedby="addon-wrapping" />
+                                                            <input type="number" id={`priceCase${index + 1}`} name={`priceCase${index + 1}`} onChange={(e) => handleChange(e, caseCombination.id, caseCombination.pricingCases)} className="form-control" placeholder="3000" aria-label="Username" aria-describedby="addon-wrapping" />
                                                         </div>
                                                     </div>
                                                 </div>
